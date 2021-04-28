8f857ca293e0e8a219381c6a1cf3b267c69046aa
Oumamem <oumamem@gmail.com>
2021-04-29 00:34:58 +0200

    patient api v1

---
+++ backend-medical/backend/src/Patient/DTO/add-patient.dto.ts
@@ -0,0 +1,10 @@
+
+
+export class AddPatientDto {
+
+    nom: string;
+    prenom: string;
+    date_naissance: string;
+    num_tel: string;
+    adresse: string;
+}
---
+++ backend-medical/backend/src/Patient/entities/patient.entity.ts
@@ -0,0 +1,8 @@
+export class Patient {
+    id: number;
+    nom: string;
+    prenom: string;
+    date_naissance: string;
+    num_tel: string;
+    adresse: string;
+}
