eebdd6fee4e1c5dffb21538fa40fd67066efe577
Oumamem <oumamem@gmail.com>
2021-05-16 00:53:16 +0200

    'auth

--- backend-medical/backend/src/User/user.controller.ts
+++ backend-medical/backend/src/User/user.controller.ts
@@ -1,6 +1,9 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 import {UserService} from "./user.service";
 import {UserSubscribeDto} from "./DTO/UserSubscribe.dto";
+import {UserLoginDTO} from "./DTO/UserLogin.dto";
+import {Patient} from "../Patient/entities/patient.entity";
+import {UserEntity} from "./entities/user.entity";
 
 @Controller('user')
 export class UserController {
@@ -10,10 +13,26 @@ export class UserController {
     }
 
 
-    @Post()
-    register(
+    @Post("register")
+    async register(
         @Body() userData: UserSubscribeDto
     ) {
         return this.userService.subscribe(userData);
     }
+    /*@Get()
+    async getUserByUsername(
+        @Body() userData: UserLoginDTO
+    ):Promise<UserEntity[]>{
+
+            return await this.userService.getUserByUsername(userData.username);
+
+    }*/
+    @Post("login")
+    async login(
+        @Body() userData: UserLoginDTO
+    )
+    {
+        return  this.userService.login(userData);
+    }
+
 }
--- backend-medical/backend/src/User/user.service.ts
+++ backend-medical/backend/src/User/user.service.ts
@@ -1,9 +1,10 @@
-import {ConflictException, Injectable} from '@nestjs/common';
+import {ConflictException, Injectable, NotFoundException} from '@nestjs/common';
 import {UserSubscribeDto} from "./DTO/UserSubscribe.dto";
 import {UserEntity} from "./entities/user.entity";
 import {Repository} from "typeorm";
 import {InjectRepository} from "@nestjs/typeorm";
 import * as bcrypt from "bcrypt";
+import {UserLoginDTO} from "./DTO/UserLogin.dto";
 
 
 @Injectable()
@@ -32,6 +33,63 @@ export class UserService {
             email: user.email,
             password: user.password
         };
+    }
+    /*async getUserByUsername(username: string): Promise<UserEntity[]>{
+        const temp= await this.userRepository.find(
+            {
+                username: username
+            }
+        )
+
+        if (!temp){
+            throw await new NotFoundException(`Username n'existe pas`);
+        }
+        else
+            return  temp;
+    }
+    async login (userData: UserLoginDTO){
+        const user= await this.getUserByUsername(userData.username)
+        console.log("userData : ", userData)
+        const hashedPassword= bcrypt.hash(userData.password)
+
+
+
+    return userData;
+
+    }*/
+    async login(credentials: UserLoginDTO)  {
+
+        // Récupére le login et le mot de passe
+        const {username, password} = credentials;
+        // On peut se logger ou via le username ou le password
+        // Vérifier est ce qu'il y a un user avec ce login ou ce mdp
+        const user = await this.userRepository.createQueryBuilder("user")
+            .where("user.username = :username or user.email = :username",
+                {username}
+            )
+            .getOne();
+        // console.log(user);
+        // Si not user je déclenche une erreur
+
+        if (!user)
+            throw new NotFoundException('username ou password erronée');
+        // Si oui je vérifie est ce que le mot est correct ou pas
+        const hashedPassword = await bcrypt.hash(password, user.salt);
+        if (hashedPassword === user.password) {
+            console.log({
+                username,
+                email: user.email,
+                role: user.role
+            })
+            return {
+                username,
+                email: user.email,
+                role: user.role
+            };
+        } else {
+            // Si mot de passe incorrect je déclenche une erreur
+            throw new NotFoundException('username ou password erronée');
+        }
     }
 
 }
